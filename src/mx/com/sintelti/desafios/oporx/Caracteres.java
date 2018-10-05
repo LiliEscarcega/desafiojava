ffe38a58e19a0b47d30957a0944d25de246d363a
Liliana Escarcega <liliescarcega_@hotmail.com>
2018-10-05 09:53:19 -0500

    ahhora si ultimos cambios

--- src/mx/com/sintelti/desafios/oporx/Caracteres.java
+++ src/mx/com/sintelti/desafios/oporx/Caracteres.java
@@ -3,7 +3,7 @@ package mx.com.sintelti.desafios.oporx;
 public class Caracteres {
 public static void main (String[] args) {
         ImplementacionJava implementacionJava = new ImplementacionJava();
-        implementacionJava.convertir("Hola Mundo o");
-
+        String cadena = implementacionJava.convertir("Hola Mundo o");
+            System.out.println(cadena);
         }
 }
--- src/mx/com/sintelti/desafios/oporx/ImplementacionJava.java
+++ src/mx/com/sintelti/desafios/oporx/ImplementacionJava.java
@@ -7,26 +7,21 @@ public class ImplementacionJava implements Solucion {
         int con = 0;
         String fin = "";
         String letra = "o";
-        String[] cadenaSeparada;
-        int total=0;
-        cadenaSeparada = entrada.split("");
+        String[] cadenaSeparada; //arreglo
+        int total=0; //contador
+        cadenaSeparada = entrada.split(""); //sepaa la palabra
         for (int i = 0; i < cadenaSeparada.length; i++) {
-            if (cadenaSeparada[i].equalsIgnoreCase(letra)) {
+            if (cadenaSeparada[i].equalsIgnoreCase(letra)) {   //ciclo que compara la letra con la o
                 String varx = "";
-                total ++;
-                for(int let = 0; let < total; let++){
+                total ++; //contador para el numero de o
+                for(int let = 0; let < total; let++){ //ciclo para contar el mumero de o
                     varx = varx + "x";
                 }
                 fin = fin + varx;
             } else {
                 fin = fin + cadenaSeparada[i];
-
             }
-
         }
-
-        System.out.println(fin);
-
-        return fin;
+        return fin; //nox retorna el valor al metodo main
        }
     }
